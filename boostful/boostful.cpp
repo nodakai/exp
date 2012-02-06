43f2f94610127c8c1030c6f697fe9dbbac05693e
NODA, Kai <nodakai@gmail.com>
2012-02-06 18:44:58 +0900

    boostful: + lexical_cast.

--- boostful/boostful.cpp
+++ boostful/boostful.cpp
@@ -2,6 +2,7 @@
 
 #include <boost/program_options.hpp>
 #include <boost/format.hpp>
+#include <boost/lexical_cast.hpp>
 
 using namespace std;
 namespace b = boost;
@@ -11,6 +12,7 @@ int main(int argc, char *argv[]) {
     po::options_description desc("Allowed options");
     desc.add_options()
         ("help", "produce help message")
+        ("lexical_cast", po::value<string>(), "tries lexical_cast")
         ("debug", po::value<int>(), "set debug level")
     ;
 
@@ -25,6 +27,12 @@ int main(int argc, char *argv[]) {
 
     if (vm.count("debug")) {
         cout << b::format("debug level == %1%") % vm["debug"].as<int>() << endl;
+    }
+
+    if (vm.count("lexical_cast")) {
+        const string before = vm["lexical_cast"].as<string>();
+        const int after = b::lexical_cast<int>(before);
+        cout << b::format("lexical_cast<int>(%1%) == %2%") % before % after << endl;
     }
 
     return 0;
