89d0a17fb06d006dafb70f9cbfeaa11157d046d8
NODA, Kai <nodakai@gmail.com>
2012-10-14 19:43:19 +0800

    enhanced gettimeofday{,Spin}.

--- gettimeofday.c
+++ gettimeofday.c
@@ -7,21 +7,48 @@
 
 #include <stdint.h>
 
+#include <getopt.h>
+
+static double s_FREQ = 3.4;
+
 static inline uint64_t rdtsc(void)
 {
     uint32_t hi, lo;
-    __asm__ __volatile__ ("rdtscp" : "=a"(lo), "=d"(hi));
+    __asm__ __volatile__ ("rdtscp" : "=a"(lo), "=d"(hi) : : "%ecx" );
     return lo | (((uint64_t)hi) << 32);
 }
 
-int main(void)
+static double myAtof(const char *str)
 {
+    char *end;
+    const long ret = strtod(str, &end);
+    if ('\0' == *end)
+        return ret;
+    fprintf(stderr, "myAtof: invalid argument [%s]", str);
+    exit(20);
+}
+
+static void printUsage(void)
+{
+    printf("gettimeofday [-f freq]\n");
+    printf("    -f freq (double): CPU frequency in GHz (defaults to 3.4)\n");
+}
+
+int main(int argc, char *argv[])
+{
+    char optchr;
+    while (-1 != (optchr = getopt(argc, argv, "f:"))) {
+        switch (optchr) {
+            case 'f': s_FREQ = myAtof(optarg); break;
+
+            default: printUsage();
+        }
+    }
+
     {
         int i;
-        for (i = 0; i < 20000000; ++i) {
-            void *p = malloc(i);
-            free(p);
-        }
+        for (i = 0; i < 20000000; ++i)
+            free(malloc(i));
     }
 
     {
@@ -29,7 +56,7 @@ int main(void)
         gettimeofday(&tv0, NULL);
         printf("sizeof(timeval) == {%u, %u}\n", (unsigned)sizeof(tv0.tv_sec), (unsigned)sizeof(tv0.tv_usec));
         gettimeofday(&tv1, NULL);
-        printf("gettimeofday(): (%lu, %lu) - (%lu, %lu) = %lu\n", tv1.tv_sec, tv1.tv_usec, tv0.tv_sec, tv0.tv_usec, (tv1.tv_sec - tv0.tv_sec) * 1000000UL + (tv1.tv_usec - tv0.tv_usec));
+        printf("gettimeofday(): (%lu, %lu) - (%lu, %lu) = %lu usec\n", tv1.tv_sec, tv1.tv_usec, tv0.tv_sec, tv0.tv_usec, (tv1.tv_sec - tv0.tv_sec) * 1000000UL + (tv1.tv_usec - tv0.tv_usec));
     }
     printf("\n");
 
@@ -46,14 +73,14 @@ int main(void)
             printf("sizeof(timespec) == {%u, %u}\n", (unsigned)sizeof(ts0.tv_sec), (unsigned)sizeof(ts0.tv_nsec));
             clock_gettime(ids[i], &ts1);
             printf("clock_getres(%d) => (%lu, %lu)\n", ids[i], res.tv_sec, res.tv_nsec);
-            printf("clock_gettime(%d): (%lu, %lu) - (%lu, %lu) = %lu\n", ids[i], ts1.tv_sec, ts1.tv_nsec, ts0.tv_sec, ts0.tv_nsec, (ts1.tv_sec - ts0.tv_sec) * 1000000000UL + (ts1.tv_nsec - ts0.tv_nsec));
+            printf("clock_gettime(%d): (%lu, %lu) - (%lu, %lu) = %lu nsec\n", ids[i], ts1.tv_sec, ts1.tv_nsec, ts0.tv_sec, ts0.tv_nsec, (ts1.tv_sec - ts0.tv_sec) * 1000000000UL + (ts1.tv_nsec - ts0.tv_nsec));
             printf("\n");
         }
     }
 
     {
         const clock_t cl = clock();
-        printf("%lu, %f\n", cl, cl / (double)CLOCKS_PER_SEC);
+        printf("CPU time spent so far is %lu units == %f secs\n", cl, cl / (double)CLOCKS_PER_SEC);
         printf("sizeof(clock_t) == %u\n", (unsigned)sizeof(cl));
     }
     printf("\n");
@@ -61,7 +88,7 @@ int main(void)
     {
         time_t tm;
         time(&tm);
-        printf("%lu\n", tm);
+        printf("%lu secs from the Epoch\n", tm);
         printf("sizeof(time_t) => %u\n", (unsigned)sizeof(tm));
     }
     printf("\n");
@@ -71,10 +98,10 @@ int main(void)
         t0 = rdtsc();
         t1 = rdtsc();
         diff = t1 - t0;
-        printf("%lu => %f\n", diff, diff / 2.93);
+        printf("%lu => %f\n", diff, diff / s_FREQ);
         t2 = rdtsc();
         diff = t2 - t1;
-        printf("%lu - %lu == %lu => %f\n", t1, t0, diff, diff / 2.93);
+        printf("%lu - %lu == %lu [cycles] => %f [ns]\n", t1, t0, diff, diff / s_FREQ);
     }
 
     return 0;
--- gettimeofdaySpin.cpp
+++ gettimeofdaySpin.cpp
@@ -11,14 +11,28 @@
 
 #include <stdint.h>
 
+#include <getopt.h>
+
 using namespace std;
 
-static inline uint32_t rdtsc(void)
+static double s_FREQ = 3.4;
+
+static inline uint64_t rdtsc(void)
 {
     uint32_t hi, lo;
-    __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
-    return lo;
-    // return lo | (((uint64_t)hi) << 32);
+    __asm__ __volatile__ ("rdtscp" : "=a"(lo), "=d"(hi) : : "%ecx" );
+    // return lo;
+    return lo | (static_cast<uint64_t>(hi) << 32);
+}
+
+static double myAtof(const char *str)
+{
+    char *end;
+    const long ret = strtod(str, &end);
+    if ('\0' == *end)
+        return ret;
+    fprintf(stderr, "myAtof: invalid argument [%s]", str);
+    exit(20);
 }
 
 int Round(int x)
@@ -30,13 +44,31 @@ int Round(int x)
         x0 /= 10;
     }
 
-    nth /= 100;
+    // nth /= 100;
     const int up = nth - 1;
-    return ((x + up)/ nth) * nth;
+    if (0 < nth)
+        return x;
+    else
+        return ((x + up) / nth) * nth;
+}
+
+static void printUsage(void)
+{
+    printf("gettimeofdaySpin [-f freq]\n");
+    printf("    -f freq (double): CPU frequency in GHz (defaults to 3.4)\n");
 }
 
 int main(int argc, char *argv[])
 {
+    char optchr;
+    while (-1 != (optchr = ::getopt(argc, argv, "f:"))) {
+        switch (optchr) {
+            case 'f': s_FREQ = myAtof(::optarg); break;
+
+            default: printUsage();
+        }
+    }
+
     long rep = 10000;
     if (argc > 1)
         rep = strtol(argv[1], NULL, 10);
@@ -48,12 +80,18 @@ int main(int argc, char *argv[])
        const uint32_t a = rdtsc(), b = rdtsc();
        if (a != b)
            arr.push_back(b - a);
-#else
+#elif 0
        struct timespec a, b;
        clock_gettime(CLOCK_REALTIME, &a);
        clock_gettime(CLOCK_REALTIME, &b);
        if (a.tv_nsec != b.tv_nsec)
            arr.push_back(b.tv_nsec - a.tv_nsec);
+#else
+       struct timeval a, b;
+       gettimeofday(&a, NULL);
+       gettimeofday(&b, NULL);
+       if (a.tv_usec != b.tv_usec)
+           arr.push_back(b.tv_usec - a.tv_usec);
 #endif
     }
 
@@ -64,6 +102,10 @@ int main(int argc, char *argv[])
     }
 
     for (Histo::const_iterator it(histo.begin()), itEnd(histo.end()); it != itEnd; ++it) {
-        cout << it->first << ", " << it->first / 2.93 << ", " << it->second << endl;
+        cout << it->first
+#if 0
+            << ", " << it->first / s_FREQ
+#endif
+            << ", " << it->second << endl;
     }
 }
