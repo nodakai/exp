ead6aa9bdaee74e6dcea7cb5412daa51df8696e9
NODA, Kai <nodakai@gmail.com>
2013-01-25 00:19:22 +0800

    fs_scrub.py: support multiple arguments.

--- fs_scrub.py
+++ fs_scrub.py
@@ -14,18 +14,16 @@ def walk(path, top=True):
                 _ = file(p).read()
 
 def main(args):
-    if len(args):
-        path = args[0]
-    else:
-        path = "."
-    # walk(path)
+    if not len(args):
+        args = (".", )
 
     t0 = time.time()
     acc = []
-    for the_dir, sub_dirs, files in os.walk(path):
-        for f in files:
-            acc.append(os.path.join(the_dir, f))
-            # print(f)
+    for path in args:
+        for the_dir, sub_dirs, files in os.walk(path):
+            for f in files:
+                acc.append(os.path.join(the_dir, f))
+                # print(f)
 
     for i in xrange(5):
         sys.stdout.write("%-15d" % (i*100/5))
@@ -33,15 +31,22 @@ def main(args):
     tot = float(len(acc))
     STEP = 1.0 / 80
     thresh = STEP
+    err_cnt = 0
+    buf = bytearray(2**20)
     for i, f in enumerate(acc):
         while i / tot > thresh:
             sys.stdout.write("-")
             sys.stdout.flush()
             thresh += STEP
-        fp = file(f)
-        while True:
-            if (len(fp.read(2**30)) < 2**30):
-                break
+        try:
+            fp = open(f)
+            while True:
+                if (fp.readinto(buf) < len(buf)):
+                    break
+        except:
+            if err_cnt > 100:
+                raise
+            err_cnt += 1
     sys.stdout.write("\n")
     t1 = time.time()
     sys.stdout.write("Took %.1f sec.\n" % (t1 - t0))
