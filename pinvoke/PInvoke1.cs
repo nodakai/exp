5c90d141bffc57206220be0566dc93658ec73b56
NODA, Kai <nodakai@gmail.com>
2013-07-14 23:06:28 +0800

    + pinvoke/PInvoke1.cs for naming an alias with EntryPoint.

---
+++ pinvoke/PInvoke1.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Runtime.InteropServices;
+
+class Prog {
+    [DllImport("libm.so", EntryPoint="sqrt")]
+    static extern double MySqrt(double x);
+
+    static void Main(string[] args) {
+        double x;
+        if (0 < args.Length && double.TryParse(args[0], out x)) {
+            Console.Out.WriteLine("sqrt({0}) == {1}", x, MySqrt(x));
+        } else {
+            Console.WriteLine("Usage: PInvoke0 num");
+            Environment.Exit(10);
+        }
+    }
+}
