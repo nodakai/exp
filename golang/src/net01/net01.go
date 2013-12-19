58ac5c633c9f7deb7cfb6070c68f8ef98e0e6ac3
NODA, Kai <nodakai@gmail.com>
2013-12-19 17:29:20 +0800

    + golang net01.go for a quick way of HTTP GET.

---
+++ golang/src/net01/net01.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "log"
+    "io/ioutil"
+)
+
+func DoTest(url string) {
+    resp, err := http.Get(url)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    body, err := ioutil.ReadAll(resp.Body)
+    resp.Body.Close()
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("[%s]\n", body)
+}
+
+func main() {
+    url0 := "http://example.net/"
+    DoTest(url0)
+
+    url1 := "http://example.nonexistent/"
+    DoTest(url1)
+}
