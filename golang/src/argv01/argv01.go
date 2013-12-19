f0f4c9dc844a62e995ee2e2b56a201d7a1735426
NODA, Kai <nodakai@gmail.com>
2013-12-19 21:47:16 +0800

    + golang argv01.go for accessing the cmdline params.

---
+++ golang/src/argv01/argv01.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+    argc := len(os.Args)
+    fmt.Printf("argc == %d\n", argc)
+    for k, arg := range os.Args {
+        fmt.Printf("%2d: [%s]\n", k, arg)
+    }
+}
