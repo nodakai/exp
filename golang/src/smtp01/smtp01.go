884d1ccc0e5c2c35a993c81bfacdab8b9dd3f546
NODA, Kai <nodakai@gmail.com>
2014-01-07 18:22:48 +0800

    + golang smtp01.go for smtp.SendMail().

---
+++ golang/src/smtp01/smtp01.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "os"
+    "log"
+    "io/ioutil"
+    "net/smtp"
+)
+
+func main() {
+    var subj []byte
+    if 1 < len(os.Args) {
+        subj = []byte("Subject: " + os.Args[1] + "\n")
+    } else {
+        subj = make([]byte, 0)
+    }
+
+    dat, err := ioutil.ReadAll(os.Stdin)
+    if err != nil {
+        log.Fatal(err)
+    }
+    dat = append(subj, dat...)
+
+    smtpSvr := "TachiColo2:25"
+    auth := smtp.Auth(nil)
+    sender := "noda@dcm-limited.com"
+    recp := []string { "nodakai@gmail.com" }
+    if err = smtp.SendMail(smtpSvr, auth, sender, recp, dat); err != nil {
+        log.Fatal(err)
+    }
+}
