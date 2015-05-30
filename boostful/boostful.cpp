00b0a28c90a2ad225938a99cb8894814ba9c40de
NODA, Kai <nodakai@gmail.com>
2015-05-30 14:49:45 +0800

    Daily comit.

--- boostful/boostful.cpp
+++ boostful/boostful.cpp
@@ -4,15 +4,43 @@
 #include <boost/format.hpp>
 #include <boost/lexical_cast.hpp>
 
+#include <boost/asio.hpp>
+
 using namespace std;
 namespace b = boost;
 namespace po = b::program_options;
+
+static void asioServer() {
+}
+
+static void asioClientHbKernel() {
+}
+
+class AsioClient {
+    AsioClient() {
+    }
+
+    ~AsioClient() {
+    }
+};
+
+static void asioClient(const string &host, const string &port) {
+    namespace ip = b::asio::ip;
+
+    b::asio::io_service ioSvc;
+    ip::tcp::socket sock(ioSvc);
+    ip::tcp::resolver resolver(ioSvc);
+    b::asio::connect(sock, resolver.resolve(ip::tcp::resolver::query(host, port)));
+}
 
 int main(int argc, char *argv[]) {
     po::options_description desc("Allowed options");
     desc.add_options()
         ("help", "produce help message")
         ("lexical_cast", po::value<string>(), "tries lexical_cast")
+        ("asio_server", "TCP server by asio")
+        ("asio_client", "TCP server by asio")
+        ("sigint", "receive SIGINT by asio")
         ("debug", po::value<int>(), "set debug level")
     ;
 
@@ -33,6 +61,25 @@ int main(int argc, char *argv[]) {
         const string before = vm["lexical_cast"].as<string>();
         const int after = b::lexical_cast<int>(before);
         cout << b::format("lexical_cast<int>(%1%) == %2%") % before % after << endl;
+    }
+
+    if (vm.count("sigint")) {
+        struct C {
+            static void handle(const b::system::error_code &e, int signal) {
+                if (e) {
+                    cout << "Error " << e << endl;
+                } else {
+                    cout << "SIGINT was caught." << endl;
+                }
+            }
+        };
+
+        b::asio::io_service ioSvc;
+        b::asio::signal_set signals(ioSvc, SIGINT);
+        signals.async_wait(&C::handle);
+        ioSvc.run();
+        cout << ": leaving..." << endl;
+        return 0;
     }
 
     return 0;
--- cpp11showcase.cpp
+++ cpp11showcase.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
+#include <stdexcept>
 
-#include <cuchar> // TestChar16
+using std::string;
+using std::cout;
+using std::endl;
 
-int main() {
-    std::cout << __func__ << std::endl;
+struct TestNullptr {
+    static void foo(int x) { cout << __func__ << ": " << x << endl; }
+    static void foo(char *x) { cout << __func__ << ": " << static_cast<void*>(x) << endl; }
+};
 
-    struct TestNullptr {
-        static void foo(int x) { std::cout << __func__ << ": " << x << std::endl; }
-        static void foo(char *x) { std::cout << __func__ << ": " << static_cast<void*>(x) << std::endl; }
-    };
-
+void testNullptr() {
     TestNullptr::foo(0);
     // TestNullptr::foo(NULL);
     TestNullptr::foo(nullptr);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct ArrayInitTemp {
+    static int x;
+
+    ArrayInitTemp() {
+        if (0 < x++)
+            throw std::logic_error("");
+    }
+
+    ArrayInitTemp() {
+    }
+};
+int ArrayInitTemp::x = 0;
+
+int main() {
+    testNullptr();
+
+
+    struct ArrayElement {
+        ArrayElement() {
+        }
+    };
+
+    ////////////////////////////////////////////////////////////////////////////////
 }
